4724f0a13e8cb356945b0c0a54f116d55066ae25
ksurajsingh <surajsinghk013@gmail.com>
2025-07-25 10:08:47 +0530

    feat(verification-smol):can identify tampered block

--- client/script.js
+++ client/script.js
@@ -132,7 +132,76 @@ function displayFromJson() {
 
 
 function verifyFromJson() {
-  document.getElementById('output').innerHTML = '<h2>Verifying JSON file blocks...</h2>';
+  const output=document.getElementById('output');
+  output.innerHTML = '<h2>Verifying JSON file blocks...</h2>';
+  const chain=document.createElement('div');
+  chain.classList.add('chain');
+
+
+ // fetching json data
+  fetch('http://localhost:8080/db/demo/personal_tampered.json')
+    .then(response => response.text())
+    .then(text=>{
+
+      // processing json data
+      // console.log("received json\n"+text);
+      const lines=text.trim().split('\n');
+      // const jsonData=lines.map(line=>JSON.parse(line));
+      // console.log("processed json\n"+jsonData);
+      // console.log(jsonData);
+      const blocks=lines.map(line=>{
+        const obj=JSON.parse(line);
+        return new Block(
+          obj.student_id,
+          obj.semester,
+          obj.subjects,
+          obj.subject_count,
+          obj.timestamp,
+          obj.prev_hash,
+          obj.hash,
+          obj.signature
+        );
+      });
+
+
+      blocks.forEach(block=>{
+        // console.log(block);
+        blockHTML=document.createElement('div')
+        block.element=blockHTML;
+        blockHTML.classList.add('block');
+        blockHTML.innerHTML=`
+        <div class='blockE'><span class='key'>Student_id: </span><span class='value'>${block.student_id}</span></div>
+        <div class='blockE'><span class='key'>Semester: </span><span class='value'>${block.semester}</span></div>
+        <div class='blockE subs'><span class='key'>Subjects: </span><span class='value'>
+          ${block.subjects.map(sub=>(`<div class='subE'>
+              <span class='key'>${sub.subject}: <span><span class='value'>${sub.mark}<span>
+              </div>`
+          )).join('')}
+          </span></div>
+        <div class='blockE'><span class='key'>Subject_count: </span><span class='value'>${block.subject_count}</span></div>
+        <div class='blockE'><span class='key'>Timestamp: </span><span class='value'>${block.timestamp}</span></div>
+        <div class='blockE'><span class='key'>Prev_Hash: </span><span class='value'>${block.prev_hash}</span></div>
+        <div class='blockE'><span class='key'>Hash: </span><span class='value'>${block.hash}</span></div>
+        <div class='blockE'><span class='key'>Signature: </span><span class='value'>${block.signature}</span></div>
+        `
+        chain.appendChild(blockHTML);
+
+
+     
+
+
+    })
+
+      for(i=1;i<blocks.length;i++){
+      if(blocks[i].prev_hash!== blocks[i-1].hash){
+        const elem=blocks[i-1].element;
+        elem.style.backgroundColor="red";
+      }
+      }
+
+  output.appendChild(chain);
+  
+})
 }
 
 
